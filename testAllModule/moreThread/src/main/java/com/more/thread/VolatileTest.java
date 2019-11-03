b8580c74886c70c0541863606b07929432cc7116
dingpfei <958033401@qq.com>
2019-11-03 22:57:04 +0800

    添加module

---
+++ testAllModule/moreThread/src/main/java/com/more/thread/VolatileTest.java
@@ -0,0 +1,31 @@
+package com.more.thread;
+
+/**
+ * @Author： Dingpengfei
+ * @Description：
+ * @Date： 2019/10/20 14:33
+ */
+public class VolatileTest {
+    private  static volatile long  count =0l;
+    private static final int NUMBER = 1000;
+
+    public static void main(String[] args) {
+        Thread substractThread = new SubtractThread();
+        substractThread.start();
+        for  (int i =0 ; i<NUMBER  ;i++) {
+            count++;
+        }
+        while (substractThread.isAlive()){
+            System.out.println("count最后的值："+count);
+        }
+    }
+
+    private static class SubtractThread extends Thread{
+        @Override
+        public   void run() {
+            for (int i= 0 ; i<NUMBER ;i++) {
+                count --;
+            }
+        }
+    }
+}
