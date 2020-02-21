89ac9db643640c5e48021a867d792c7c488e29dc
dingpf1 <dingpfei@qq.com>
2020-02-21 13:54:30 +0800

    test

---
+++ dubbodemo/dubbo-customer/src/main/java/com/dubbo/demo/customer/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.dubbo.demo.customer.service;
+
+/**
+ * @Author： Dingpengfei
+ * @Description：
+ * @Date： 2020-2-1 16:18
+ */
+public interface CustomerService {
+    void dubboCustomer();
+}
---
+++ testAllModule/moreThread/src/main/java/com/more/thread/Test.java
@@ -0,0 +1,13 @@
+package com.more.thread;
+
+/**
+ * @Author： Dingpengfei
+ * @Description：
+ * @Date： 2020-2-21 11:25
+ */
+public class Test {
+    public static void main(String[] args) {
+        Integer a=null;
+        System.out.println(a.intValue());
+    }
+}
