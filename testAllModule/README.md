89edfcb97593b4811204e2f8c595fd9a73d41e8a
dingpf <95803401@qq.com>
2020-06-15 23:00:43 +0800

    生成6位的验证码

--- testAllModule/README.md
+++ testAllModule/README.md
@@ -4,3 +4,13 @@
 * 我们在使用mybatis的时候，后面需要加上jdbcType 例如在前面的那个如果数据库为null的话，mybatis会报错的。  因为没有对null处理
 ，即使 又加了 nvl是针对数据库层面的null。 _但是mybatis层面的不写的话，他是不知道这个字段的类型的_。 
 >(#{snMinValue, jdbcType=INTEGER}, 0)           (#{snMinValue}, 0) 
+
+###估算线程数
+*  线程数=cpu可用核数 / 1-阻塞系数（io密集型接近于1，计算密集型接近于0）
+### 提升qps
+*  提高并发数
+    .开多线程
+    .增加连接数  tomcat  redis  mysql
+    .服务无状态，横向扩张
+    .让服务的能力对等
+*  减少响应时间
--- testAllModule/test-module/src/main/java/com/mo/empty/EmptyTest.java
+++ testAllModule/test-module/src/main/java/com/mo/empty/EmptyTest.java
@@ -6,5 +6,13 @@ package com.mo.empty;
  * @Date： 2020-5-22 9:54
  */
 public class EmptyTest {
+    public static void main(String[] args) {
+        //字符串的操作比较慢
+        String cod1e =(Math.random()+"").substring(2,10);
+        //比较快 因为都是字符的运算
+        String code = String.valueOf((int)((Math.random()*9+1)*Math.pow(10,5)));
+        System.out.println(Math.pow(10,5));
+        System.out.println(code);
+    }
 
 }
