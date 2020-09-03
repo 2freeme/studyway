8067ecec0a953b98388a722120886f5e2210baf1
dingpf1 <dingpfei@qq.com>
2020-09-03 17:13:36 +0800

    spring stat machine

--- testAllModule/goudaner-platform-master/src/main/java/com/goudaner/platform/common/RestControllerResponseAdvice.java
+++ testAllModule/goudaner-platform-master/src/main/java/com/goudaner/platform/common/RestControllerResponseAdvice.java
@@ -1,6 +1,5 @@
 package com.goudaner.platform.common;
 
-import com.alibaba.fastjson.JSON;
 import com.goudaner.platform.base.SyCode;
 import com.goudaner.platform.base.SyResult;
 import org.springframework.core.MethodParameter;
@@ -10,8 +9,6 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
-import java.io.Serializable;
 
 @ControllerAdvice
 public class RestControllerResponseAdvice implements ResponseBodyAdvice<Object> {
@@ -28,7 +25,7 @@ public class RestControllerResponseAdvice implements ResponseBodyAdvice<Object> 
         // 对body进行封装处理
         if (body instanceof String) {
             String msg = (String) body;
-            SyResult resultJson = new SyResult(SyCode.OK,msg);
+            SyResult<String> resultJson = new SyResult(SyCode.OK,msg);
             // 因为在controller层中返回的是String类型，这边如果换成SyResult的话，会导致StringMessageConverter方法类型转换异常，所以这边将对象转成字符串
             return resultJson;
         } else if (body instanceof Object) {
